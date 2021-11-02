aad84f26db9bd1fe9d169046694c503f8f1dc68e
austinh2001 <austinh2001@gmail.com>
2021-11-02 00:25:22 -0400

    Add files via upload

---
+++ MultiCoAdd.m
@@ -0,0 +1,8 @@
+function [summated_image_data] = MultiCoAdd(image_data_array)
+    image_data_array_size = size(image_data_array);
+    summated_image_data = zeros(image_data_array_size(1),image_data_array_size(2));
+    for i=1:image_data_array_size(3)
+        summated_image_data = CoAdd(summated_image_data,image_data_array(:,:,i));
+    end
+end
+
--- createImageDataArray.m
+++ createImageDataArray.m
@@ -3,11 +3,15 @@ function [image_data_array] = createImageDataArray(folder_filepath)
         folder_filepath = uigetdir;
     end
     folder_files = dir(fullfile(folder_filepath,'*.fit'));
-    folder_filenames = zeros(size(folder_files))
-    for k = 1:length(folder_files)
-        baseFileName = folder_files(k).name;
-        folder_filenames(k) = baseFileName;
-        %fullFileName = fullfile(folder_filepath, baseFileName);
+    folder_filenames = strings(size(folder_files));
+    for i = 1:length(folder_files)
+        baseFileName = folder_files(i).name;
+        folder_filenames(i) = baseFileName;
     end
-    folder_filenames
+    template_fits_file = rfits(folder_filepath + folder_filenames(1));
+    image_data_array = zeros(size(template_fits_file.data));
+    for j = 1:length(folder_filenames)
+        fits_file = rfits(folder_filepath + folder_filenames(j));
+        image_data_array(:,:,j) = fits_file.data;
+    end
 end
