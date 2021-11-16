8834f75d2417872a12f56d29c91a472f8967787a
austinh2001 <austinh2001@gmail.com>
2021-11-16 12:30:10 -0500

    Added a functionality to displayAdjustedImage

--- displayAdjustedImage.m
+++ displayAdjustedImage.m
@@ -4,14 +4,18 @@ image_data = fliplr(image_data);
 %Modified version of:
 %https://www.mathworks.com/matlabcentral/answers/42434-how-to-increase-the-contrast-in-an-image-using-imagesc-with-removal-of-outliers
 if (nargin==1), numOfSigma=3; end
-ndev = numOfSigma;
-data_mean = mean(image_data(:));
-data_std = std(image_data(:));
-data_min = min(image_data(:));
-data_max = max(image_data(:));
-cmin = max(data_min, data_mean - ndev * data_std );
-cmax = min(data_max, data_mean + ndev * data_std );
-imagesc(image_data,[cmin, cmax])
+if(numOfSigma >= 0)
+    ndev = numOfSigma;
+    data_mean = mean(image_data(:));
+    data_std = std(image_data(:));
+    data_min = min(image_data(:));
+    data_max = max(image_data(:));
+    cmin = max(data_min, data_mean - ndev * data_std );
+    cmax = min(data_max, data_mean + ndev * data_std );
+    imagesc(image_data,[cmin, cmax])
+else
+    imshow(image_data)
+end
 axis image
 set(gca,'YDir','normal') 
 end
