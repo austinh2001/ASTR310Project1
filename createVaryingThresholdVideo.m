42065d96050bd5334d58c1e7167a28e95490712f
austinh2001 <austinh2001@gmail.com>
2021-11-16 15:21:56 -0500

    Changed threshold video code

--- createVaryingThresholdVideo.m
+++ createVaryingThresholdVideo.m
@@ -1,11 +1,11 @@
-function [] = createVaryingThresholdVideo(image_data,maxNumSigma,video_length)
-totalFrames = 1000;
+function [] = createVaryingThresholdVideo(image_data,noise_region,maxNumSigma,video_length)
+totalFrames = 100;
 sigmas = linspace(0,maxNumSigma,totalFrames);
 writerObj = VideoWriter('Thresholds_' + string(maxNumSigma) + '.avi');
 writerObj.FrameRate = length(sigmas)/video_length;
 open(writerObj);
 for i=1:length(sigmas)
-    threshold_image = threshE(image_data,sigmas(i));
+    threshold_image = threshE(image_data,noise_region,sigmas(i));
     writeVideo(writerObj, getframe(gcf))
     display(string((i/totalFrames)*100) + " %")
 end
